66f524543b1834b42bc405108a3711c1d22dc8f5
forafish <fora.luo@gmail.com>
2018-04-26 19:12:37 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 require = require("esm")(module, { debug: true });
-console.time('mostly-feathers-cache import');
 module.exports = require('./src/index').default;
-console.timeEnd('mostly-feathers-cache import');
